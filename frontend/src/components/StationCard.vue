e61d9269d8a0a39a24e6bda7ca8902488bfd8435
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-24 16:28:30 +0100

    feat(StationCard): add StationCard component to display station information

---
+++ frontend/src/components/StationCard.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    station: {
+        type: [Object, Array],
+        required: true
+    }
+})
+</script>
+<template>
+    <div class="border rounded-xl p-4 shadow">
+        <h2>{{ station.name }}</h2>
+        <p>{{ station }}</p>
+    </div>
+</template>
