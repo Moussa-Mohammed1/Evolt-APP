40d501f372c71137eecbe20532ec54c38fa3e4cd
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-24 17:24:58 +0100

    feat(router): add user route and update login/register meta to hide navbar

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,11 +3,14 @@ import HomeView from '../views/HomeView.vue';
 import LoginView from '../views/LoginView.vue';
 import RegisterView from '../views/RegisterView.vue';
 import StationsViews from '../views/StationsView.vue';
+import UsersView from '../views/UsersView.vue';
+
 const routes = [
     { path: '/', name: 'home', component: HomeView },
-    { path: '/login', name: 'login', component: LoginView },
-    { path: '/register', name: 'register', component: RegisterView },
+    { path: '/login', name: 'login', component: LoginView, meta: { hideNavbar: true } },
+    { path: '/register', name: 'register', component: RegisterView, meta: { hideNavbar: true } },
     { path: '/stations', name: 'stations', component: StationsViews },
+    { path: '/users', name: 'users', component: UsersView },
 ];
 
 const router = createRouter({
